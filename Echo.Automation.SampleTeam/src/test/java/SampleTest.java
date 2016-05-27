bdc407b8a9c6d652bf7395b934363001d8cb598a
SalGandara <salvador_gandara@ultimatesoftware.com>
2016-05-27 10:29:19 -0400

    Corrected SampleTest version

--- Echo.Automation.SampleTeam/src/test/java/SampleTest.java
+++ Echo.Automation.SampleTeam/src/test/java/SampleTest.java
@@ -2,7 +2,7 @@ import echo.core.common.web.BrowserSize;
 import echo.core.common.web.BrowserSizeMap;
 import echo.selenium.SeleniumCookie;
 import main.Sample;
-import org.junit.AfterClass;
+import org.junit.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,19 +16,60 @@ public class SampleTest {
 
     private static Sample product;
 
-    @BeforeClass
+    @Before
     public static void fixtureSetUp() {
         product = Launch(Sample.class, Firefox);
         product.Browser.GoToUrl("http://gandaras01web.newgen.corp/");
     }
 
-    @AfterClass
+    @After
     public static void fixtureTearDown() {
         product.Browser.Quit();
     }
 
     @Test
-    public void SampleTest() {
+    public void TestGoBackGoForward_01() {
+        product.Login.UserNameTextBox.Set("usa-canu");
+        product.Login.PasswordTextBox.Set("password");
+        product.Login.LoginButton.Click();
+        product.Browser.GoBack();
+        product.Browser.GoForward();
+    }
+
+    @Test
+    public void TestClearAndRefresh_02()
+    {
+        product.Login.UserNameTextBox.Set("usa-canu");
+        product.Login.PasswordTextBox.Set("password");
+        product.Login.UserNameTextBox.Clear();
+        product.Login.UserNameTextBox.Set("usa-canu");
+        product.Browser.Refresh();
+    }
+
+    @Test
+    public void TestWindowResizing_03(){
+        product.Login.UserNameTextBox.Set("usa-canu");
+        product.Login.PasswordTextBox.Set("password");
+        product.Login.LoginButton.Click();
+        product.Browser.Resize(BrowserSizeMap.Map(BrowserSize.TabletLandscape));
+        product.Browser.Resize(BrowserSizeMap.Map(BrowserSize.SmallTabletLandscape));
+        product.Browser.Resize(BrowserSizeMap.Map(BrowserSize.MobileLandscape));
+        product.Browser.Maximize();
+
+    }
+
+    @Test
+    public void TestDoubleClickScrollTopEnd_04()
+    {
+        product.Login.UserNameTextBox.Set("usa-canu");
+        product.Login.PasswordTextBox.Set("password");
+        product.Login.LoginButton.DoubleClick();
+        product.Browser.GoToUrl("http://www.tutorialspoint.com");
+        product.Browser.
+    }
+    @Test
+    public void SampleTest2()
+    {
         product.Browser.Resize(BrowserSizeMap.Map(BrowserSize.TabletLandscape));
         product.Browser.Resize(BrowserSizeMap.Map(BrowserSize.SmallTabletLandscape));
         product.Browser.Resize(BrowserSizeMap.Map(BrowserSize.MobileLandscape));
@@ -40,13 +81,10 @@ public class SampleTest {
         product.Browser.Refresh();
         product.Login.UserNameTextBox.Set("usa-canu");
         product.Login.PasswordTextBox.Set("password");
-        product.Login.LoginButton.Click();
-        product.Browser.GoToUrl("http://www.google.com");
+        product.Login.LoginButton.DoubleClick();
+        //product.Login.LoginButton.Click();
+        //product.Browser.GoToUrl("http://www.google.com");
         product.Browser.GoBack();
         product.Browser.GoForward();
-        //scroll tests
-        product.Browser.GoToUrl("http://www.tutorialspoint.com/");
-        product.Browser.ScrollToEnd();
-        product.Browser.ScrollToTop();
     }
 }
