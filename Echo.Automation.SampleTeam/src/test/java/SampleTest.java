130675d55bfb4061b22e1109ae2157ad74db72b5
Salvador Gandara <salvador_gandara@ultimatesoftware.com>
2016-06-10 13:04:50 -0400

    Switched Test Browser to Chrome to test drivers in CI, fail expected

--- Echo.Automation.SampleTeam/src/test/java/SampleTest.java
+++ Echo.Automation.SampleTeam/src/test/java/SampleTest.java
@@ -7,6 +7,7 @@ import org.junit.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static echo.core.common.web.BrowserType.Chrome;
 import static echo.core.common.web.BrowserType.Firefox;
 import static echo.core.test_abstraction.product.Echo.Launch;
 
@@ -19,7 +20,7 @@ public class SampleTest {
 
     @Before
     public void SetUp() {
-        product = Launch(Sample.class, Firefox);
+        product = Launch(Sample.class, Chrome);
         product.Browser.GoToUrl("http://gandaras01web.newgen.corp/");
     }
 
