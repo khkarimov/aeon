1ec98549b15590ea41d9d5952882f2e395ca1c30
Sebastian Restrepo <Sebastian_Restrepo@ultimatesoftware.com>
2016-12-28 17:03:11 -0500

    Made grid classes abstract to ensure users extend the classes

--- Echo.Core/src/main/java/echo/core/test_abstraction/elements/web/Grid.java
+++ Echo.Core/src/main/java/echo/core/test_abstraction/elements/web/Grid.java
@@ -2,15 +2,14 @@ package echo.core.test_abstraction.elements.web;
 
 import echo.core.command_execution.AutomationInfo;
 import echo.core.common.web.interfaces.IBy;
-import echo.core.common.web.selectors.By;
 
 /**
  * Created by AdamC on 4/13/2016.
  */
-public class Grid<T extends RowActions> extends WebElement {
+public abstract class Grid<T extends RowActions> extends WebElement {
     public T RowBy;
     
-    public Grid(AutomationInfo info, By selector, T rowBy) {
+    public Grid(AutomationInfo info, IBy selector, T rowBy) {
         super(info, selector);
         this.RowBy = rowBy;
     }
--- Echo.Core/src/main/java/echo/core/test_abstraction/elements/web/RowElements.java
+++ Echo.Core/src/main/java/echo/core/test_abstraction/elements/web/RowElements.java
@@ -2,13 +2,11 @@ package echo.core.test_abstraction.elements.web;
 
 import echo.core.command_execution.AutomationInfo;
 import echo.core.common.web.interfaces.IBy;
-import echo.core.common.web.interfaces.IByJQuery;
-import echo.core.common.web.selectors.By;
 
 /**
  * Created by AdamC on 4/13/2016.
  */
-public class RowElements {
+public abstract class RowElements {
     protected AutomationInfo automationInfo;
     protected IBy selector;
 
