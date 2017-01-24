e734017c1aef350c3f1b92478621fe9193c337ce
Gabriel Perez Clark <gaboperezclark@gmail.com>
2017-01-24 15:03:18 -0500

    Adding unit test to Echo.Core for pipeline.

---
+++ Echo.Core/src/test/java/UnitTests.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+
+/**
+ * Created by josepe on 1/24/2017.
+ */
+
+public class UnitTests {
+    @Test
+    public void dummyTestPass(){
+        boolean dummyVariable = true;
+        assert(dummyVariable);
+    }
+}
