79618b6adc0bbf6d6405db4dac77d7148fb501e6
Gabriel Perez Clark <gaboperezclark@gmail.com>
2017-04-04 18:27:59 -0400

    Added script to handle JSONP requests
    Also added the script to the source page and it wont be injected from there anymore

--- Aeon.Core/src/main/java/aeon/core/common/helpers/AjaxWaiter.java
+++ Aeon.Core/src/main/java/aeon/core/common/helpers/AjaxWaiter.java
@@ -37,74 +37,17 @@ public class AjaxWaiter {
         return timeout.getMillis();
     }
 
-    public void injectJS() {
-        try {
-            String InjectScriptTag = "var a = document.createElement('script');a.text=\"" +
-                    getAjaxWaiterJS() + "\";a.setAttribute('id', 'aeon.ajax.waiter.script');document.body.appendChild(a);";
-            webDriver.executeScript(InjectScriptTag);
-            System.out.println("Injected JS");
-        }catch (ScriptExecutionException e){
-            System.out.println("Could not inject JS");
-        }
-    }
-
     public void waitForAsync() {
         long count;
         DateTime end = clock.getUtcNow().withDurationAdded(timeout.getMillis(), 1);
         do{
             try {
-                count = (long) webDriver.executeScript("return aeonAjaxCounter");
+                count = (long) webDriver.executeScript("return aeon.ajaxCounter;");
             }catch(ScriptExecutionException e){
-                injectJS();
                 return;
             }
             Sleep.waitInternal();
         }while(count != 0  && clock.getUtcNow().isBefore(end.toInstant()));
     }
 
-    private final static String getAjaxWaiterJS(){
-        return  "var aeonAjaxCounter = 0;\n" +
-                "(function(open) {\n" +
-                "            var asyncCallOccurred = false;\n" +
-                "            var event;\n" +
-                "\n" +
-                "            if (document.createEvent) {\n" +
-                "                event = document.createEvent('HTMLEvents');\n" +
-                "                event.initEvent('aeon.async.done', true, true);\n" +
-                "            } else {\n" +
-                "                event = document.createEventObject();\n" +
-                "                event.eventType = 'aeon.async.done';\n" +
-                "            }\n" +
-                "\n" +
-                "            event.eventName = 'aeon.async.done';\n" +
-                "\n" +
-                "            document.getElementById('start').addEventListener('aeon.async.done', function() {\n" +
-                "                document.getElementById('start').setAttribute('data-async', 'done');\n" +
-                "            });\n" +
-                "\n" +
-                "            XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {\n" +
-                "                $('#counter').html(++aeonAjaxCounter);\n" +
-                "\n" +
-                "                var readyStateChange = function() {\n" +
-                "                    if (this.readyState === 4) {\n" +
-                "                        $('#counter').html(--aeonAjaxCounter);\n" +
-                "\n" +
-                "                        if(aeonAjaxCounter === 0) {\n" +
-                "                            setTimeout(function() {\n" +
-                "                                if (document.createEvent) {\n" +
-                "                                    document.getElementById('start').dispatchEvent(event);\n" +
-                "                                } else {\n" +
-                "                                    document.getElementById('start').fireEvent('on' + event.eventType, event);\n" +
-                "                                }\n" +
-                "                            }, 1);\n" +
-                "                        }\n" +
-                "                    }\n" +
-                "                };\n" +
-                "\n" +
-                "                this.addEventListener('readystatechange', readyStateChange, false);\n" +
-                "\n" +
-                "                open.call(this, method, url, async, user, pass);\n" +
-                "            };\n" +
-                "\n" +
-                "        })(XMLHttpRequest.prototype.open);";}
 }
