120d7c2f1a3e014b0350408cd1ed140cc215a670
Rohan Patel <rohan_patel@ultimatesoftware.com>
2017-05-10 16:14:20 -0400

    TOOL-6164:Added EnabledCommand Unit Test

---
+++ Aeon.Core/src/test/java/TestAbstraction/aeon/core/command/execution/commands/web/EnabledCommandTests.java
@@ -0,0 +1,63 @@
+package TestAbstraction.aeon.core.command.execution.commands.web;
+
+import aeon.core.command.execution.commands.initialization.ICommandInitializer;
+import aeon.core.command.execution.commands.web.EnabledCommand;
+import aeon.core.common.web.interfaces.IBy;
+import aeon.core.framework.abstraction.controls.web.WebControl;
+import aeon.core.framework.abstraction.drivers.IDriver;
+import aeon.core.framework.abstraction.drivers.IWebDriver;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.function.Consumer;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by rohanp on 5/10/17.
+ */
+public class EnabledCommandTests {
+
+    private EnabledCommand command;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    IWebDriver driver;
+
+    @Mock
+    WebControl control;
+
+    @Mock
+    private Consumer<IDriver> driverConsumer;
+
+    @Mock
+    private ICommandInitializer commandInitializer;
+
+    @Mock
+    IBy selector;
+
+    @Before
+    public void setUp()
+    {
+        command = new EnabledCommand(selector, commandInitializer);
+    }
+
+    @Test
+    public void enabledCommand_CallsExecute()
+    {
+        when(commandInitializer.setContext()).thenReturn(driverConsumer);
+        when(commandInitializer.findElement(driver, selector)).thenReturn(control);
+        Consumer<IDriver> action = command.getCommandDelegate();
+        action.accept(driver);
+        verify(driver, times(1)).isElementEnabled(control);
+    }
+
+}
