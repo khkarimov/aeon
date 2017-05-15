fc079e3f7fd95fa798154e6f50bbe4b70ebfeea7
Bryan Taylor <bryant@bryan-taylor.us.corp>
2017-05-15 12:45:06 -0400

    TOOL-6187: Adding unit tests for JavaScriptCommands

---
+++ Aeon.Core/src/test/java/aeon/core/command/execution/commands/web/SetTextValueByJavaScriptCommandTests.java
@@ -0,0 +1,65 @@
+package aeon.core.command.execution.commands.web;
+
+import aeon.core.command.execution.commands.initialization.ICommandInitializer;
+import aeon.core.common.web.interfaces.IBy;
+import aeon.core.framework.abstraction.controls.web.WebControl;
+import aeon.core.framework.abstraction.drivers.IWebDriver;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by bryant on 5/15/17.
+ */
+public class SetTextValueByJavaScriptCommandTests {
+    private SetTextByJavaScriptCommand setTextByJavaScriptCommandObject;
+    private String value;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Mock
+    private IBy selector;
+
+    @Mock
+    private ICommandInitializer initializer;
+
+    @Mock
+    private IWebDriver driver;
+
+    @Mock
+    private WebControl control;
+
+    @Before
+    public void setup(){
+        setTextByJavaScriptCommandObject = new SetTextByJavaScriptCommand(selector, initializer, value);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalArguementThrownWhenDriverIsNull(){
+        // Act
+        setTextByJavaScriptCommandObject.commandDelegate(null, control);
+
+        // Assert
+        thrown.expectMessage("driver");
+    }
+
+    @Test
+    public void commandDelegateExecutesSetDivValueByJavaScript(){
+        // Act
+        setTextByJavaScriptCommandObject.commandDelegate(driver, control);
+
+        // Assert
+        verify(driver, times(1)).setTextByJavaScript(control, value);
+    }
+}
