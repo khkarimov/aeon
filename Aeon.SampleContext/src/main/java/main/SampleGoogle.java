4faaf686101b1b606f151627c3e2751b1b82c444
Jake Leventhal <Jake_Leventhal@ultimatesoftware.com>
2017-06-06 10:17:33 -0400

    deleted comments

--- Aeon.SampleContext/src/main/java/main/SampleGoogle.java
+++ Aeon.SampleContext/src/main/java/main/SampleGoogle.java
@@ -6,9 +6,6 @@ import aeon.core.testabstraction.elements.web.*;
 import main.samplegrid.MyGrid;
 import main.samplegrid.MyGridHeaders;
 
-/**
- * Created by jacoble on 6/5/2017.
- */
 public class SampleGoogle {
     private AutomationInfo info;
 
