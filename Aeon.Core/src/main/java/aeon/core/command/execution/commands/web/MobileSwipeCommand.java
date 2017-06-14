b3ca2ddd7afc4f498a7ac92e5a1b5accd82caa28
Jad Zeineddine <zeineddine.jad@gmail.com>
2017-06-14 14:53:40 -0400

    stuff

--- Aeon.Core/src/main/java/aeon/core/command/execution/commands/web/MobileSwipeCommand.java
+++ Aeon.Core/src/main/java/aeon/core/command/execution/commands/web/MobileSwipeCommand.java
@@ -1,17 +1,11 @@
 package aeon.core.command.execution.commands.web;
 
 import aeon.core.command.execution.commands.Command;
-import aeon.core.command.execution.commands.initialization.ICommandInitializer;
 import aeon.core.common.Resources;
-import aeon.core.common.web.interfaces.IBy;
-import aeon.core.framework.abstraction.controls.web.WebControl;
 import aeon.core.framework.abstraction.drivers.IDriver;
 import aeon.core.framework.abstraction.drivers.IWebDriver;
 
-import java.util.ArrayList;
-import java.util.Locale;
-
-public class MobileSwipesCommand extends Command {
+public class MobileSwipeCommand extends Command {
 
     private int startx;
     private int starty;
@@ -20,9 +14,9 @@ public class MobileSwipesCommand extends Command {
     private int duration;
 
     /**
-     * Initializes a new instance of the {@link MobileSwipesCommand} class.
+     * Initializes a new instance of the {@link MobileSwipeCommand} class.
      */
-    public MobileSwipesCommand(int startx, int starty, int endx, int endy, int duration) {
+    public MobileSwipeCommand(int startx, int starty, int endx, int endy, int duration) {
         super(Resources.getString("MobileSwipesCommand_Info"));
         this.startx = startx;
         this.starty = starty;
---
+++ Aeon.Core/src/test/java/aeon/core/command/execution/commands/web/MobileSwipeCommandTests.java
@@ -0,0 +1,69 @@
+package aeon.core.command.execution.commands.web;
+
+
+import aeon.core.command.execution.commands.initialization.ICommandInitializer;
+import aeon.core.common.web.interfaces.IBy;
+import aeon.core.framework.abstraction.controls.web.WebControl;
+import aeon.core.framework.abstraction.drivers.IDriver;
+import aeon.core.framework.abstraction.drivers.IWebDriver;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.function.Consumer;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MobileSwipeCommandTests {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @Mock
+    WebControl control;
+    @Mock
+    private Consumer<IDriver> driverConsumer;
+    @Mock
+    private IWebDriver driver;
+    @Mock
+    private ICommandInitializer commandInitializer;
+
+    @Mock
+    private IBy selector;
+
+    private MobileSwipeCommand command;
+    private int startx;
+    private int starty;
+    private int endx;
+    private int endy;
+    private int duration;
+
+    @Before
+    public void setUp() {
+        startx = 1;
+        starty = 1;
+        endx = 2;
+        endy = 2;
+        duration = 1;
+        command = new MobileSwipeCommand(startx, starty, endx, endy, duration);
+    }
+
+    @Test
+    public void MobileSwipeCommand_CallsExecute() {
+        //Arrange
+        when(commandInitializer.setContext()).thenReturn(driverConsumer);
+        when(commandInitializer.findElement(driver, selector)).thenReturn(control);
+
+        //Act
+        Consumer<IDriver> action = command.getCommandDelegate();
+        action.accept(driver);
+
+        //Assert
+        verify(driver, times(1)).mobileSwipes(startx, starty, endx, endy, duration);
+    }
+
+}
