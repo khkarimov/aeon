a8d089637fc9f68e8fa00cccb7e0957912cab823
Terry Jean <terrymiami2@aol.com>
2017-06-29 11:24:49 -0400

    Added a method to clean a Perfecto app when launching.

--- Aeon.Selenium/src/main/java/aeon/selenium/SeleniumAdapterFactory.java
+++ Aeon.Selenium/src/main/java/aeon/selenium/SeleniumAdapterFactory.java
@@ -186,6 +186,20 @@ public final class SeleniumAdapterFactory implements IAdapterExtension {
                 capabilities = (DesiredCapabilities) getCapabilities();
                 driver = new AndroidDriver(seleniumHubUrl, capabilities);
                 ((AndroidDriver) driver).context(driverContext);
+
+                //Perfecto only - Cleans the app data for a fresh new session.
+                if (!perfectoUser.isEmpty() && !perfectoPass.isEmpty() && ) {
+
+                    //Clean command
+                    Map<String, Object> cleanParams = new HashMap<>();
+                    cleanParams.put("identifier", appPackage);
+                    ((AndroidDriver) driver).executeScript("mobile:application:clean", cleanParams);
+
+                    //Re-opens the application
+                    Map<String, Object> openParams = new HashMap<>();
+                    openParams.put("identifier", appPackage);
+                    ((AndroidDriver) driver).executeScript("mobile:application:open", openParams);
+                }
                 break;
 
             default:
