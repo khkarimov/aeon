2a561e6dfc3045fc889f14255b8f5dd38ce0bae6
Ricardo Garcia <ricardo_garcia@ultimatesoftware.com>
2018-01-05 17:04:27 -0500

    TOOL-7161-ThrottleConfig test enumeration errors, changed the log.info to match aeon.properties

--- Aeon.Core/src/test/java/aeon/core/testabstraction/product/ConfigurationTests.java
+++ Aeon.Core/src/test/java/aeon/core/testabstraction/product/ConfigurationTests.java
@@ -168,7 +168,16 @@ public class ConfigurationTests {
         config.loadConfiguration();
 
         //Assert
-        verify(log, times(1)).info("These are the properties values currently in use:\naeon.timeout = 10\naeon.wait_for_ajax_responses = true\naeon.timeout.ajax = 20\naeon.scroll_element_into_view = false\n");
+        verify(log, times(1)).info("These are the properties values currently in use:\naeon.timeout = 10 \naeon.wait_for_ajax_responses = true \naeon.throttle = 100\naeon.timeout.ajax = 20\naeon.scroll_element_into_view = false \n");
+      /* info(
+                "These are the properties values currently in use:
+                aeon.timeout = 10
+                aeon.wait_for_ajax_responses = true
+                aeon.throttle = 100
+                aeon.timeout.ajax = 20
+                aeon.scroll_element_into_view = false
+        */
+
     }
 
     @Test
