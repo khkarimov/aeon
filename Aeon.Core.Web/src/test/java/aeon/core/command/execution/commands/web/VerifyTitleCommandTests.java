d24014e042a012fe98a197c359f07aa6fc63b73b
Lam Pui Chan <lam_chan@ultimatesoftware.com>
2018-01-16 17:22:57 -0500

    Unit Tests - SelectFileCommand, VerifyTitleCommand

---
+++ Aeon.Core.Web/src/test/java/aeon/core/command/execution/commands/web/VerifyTitleCommandTests.java
@@ -0,0 +1,51 @@
+package aeon.core.command.execution.commands.web;
+
+import aeon.core.framework.abstraction.drivers.IWebDriver;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class VerifyTitleCommandTests {
+
+    private VerifyTitleCommand verifyTitleCommand;
+    private String comparingText = "";
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private IWebDriver driver;
+
+    @Before
+    public void setUp() {
+        verifyTitleCommand = new VerifyTitleCommand(comparingText);
+    }
+
+    @Test
+    public void commandDelegateVerifyTitleCommand() {
+        //Arrange
+
+        //Act
+        verifyTitleCommand.driverDelegate(driver);
+
+        //Assert
+        verify(driver,times(1)).verifyTitle(comparingText);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void driverNullThrowsException(){
+        //Arrange
+
+        //Act
+        verifyTitleCommand.driverDelegate(null);
+
+        //Assert
+
+    }
+}
