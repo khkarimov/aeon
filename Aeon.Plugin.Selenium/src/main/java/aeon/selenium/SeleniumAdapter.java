c8b64a2b66e327f0761f4abc3ad6d6611f182bb6
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-09-18 14:13:51 -0400

    TOOL-7642: Added check for html5 and using appropiate drag and drop for each case

--- Aeon.Plugin.Selenium/src/main/java/aeon/selenium/SeleniumAdapter.java
+++ Aeon.Plugin.Selenium/src/main/java/aeon/selenium/SeleniumAdapter.java
@@ -900,26 +900,49 @@ public class SeleniumAdapter implements IWebAdapter, AutoCloseable {
         if (webDriver == null) {
             throw new IllegalStateException("The driver is null.");
         }
-        log.trace("Executing drag and drop with javascript script");
         WebElement drop = ((SeleniumElement) dropElement).getUnderlyingWebElement();
         WebElement target = ((SeleniumElement) findElement(targetElement)).getUnderlyingWebElement();
 
-        executeScript("function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
-                + "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n" + "data: {},\n"
-                + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
-                + "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n" + "return event;\n"
-                + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
-                + "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
-                + "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
-                + "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n" + "}\n"
-                + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
-                + "var dragStartEvent =createEvent('dragstart');\n" + "dispatchEvent(element, dragStartEvent);\n"
-                + "var dropEvent = createEvent('drop');\n"
-                + "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
-                + "var dragEndEvent = createEvent('dragend');\n"
-                + "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
-                + "var source = arguments[0][0];\n" + "var destination = arguments[0][1];\n"
-                + "simulateHTML5DragAndDrop(source, destination)", drop, target);
+        if (isHtml5Draggable(drop)) {
+            log.trace("Executing drag and drop with javascript script");
+            executeScript("function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
+                    + "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n" + "data: {},\n"
+                    + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
+                    + "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n" + "return event;\n"
+                    + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
+                    + "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
+                    + "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
+                    + "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n" + "}\n"
+                    + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
+                    + "var dragStartEvent =createEvent('dragstart');\n" + "dispatchEvent(element, dragStartEvent);\n"
+                    + "var dropEvent = createEvent('drop');\n"
+                    + "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
+                    + "var dragEndEvent = createEvent('dragend');\n"
+                    + "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
+                    + "var source = arguments[0][0];\n" + "var destination = arguments[0][1];\n"
+                    + "simulateHTML5DragAndDrop(source, destination)", drop, target);
+        } else {
+            log.trace("new Actions(IWebDriver).dragAndDrop(IWebElement, IWebElement);");
+            Actions builder = new Actions(webDriver);
+
+            builder.clickAndHold(drop).perform();
+            Sleep.wait(250);
+
+            if (browserType == BrowserType.Firefox) {
+                scrollElementIntoView(targetElement);
+            }
+
+            builder.moveToElement(target).perform();
+            builder.release(target).perform();
+        }
+    }
+
+    private boolean isHtml5Draggable(WebElement drop) {
+        String draggable = drop.getAttribute("draggable");
+        if (draggable == null) {
+            return false;
+        }
+        return draggable.equalsIgnoreCase("true");
     }
 
     /**
