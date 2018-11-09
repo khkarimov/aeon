103ba9670b53f5f7f7fea7f1aa41a2057ae02e8e
Nicolette Celli <nicolette_celli@ultimatesoftware.com>
2018-11-09 09:48:17 -0500

    TOOL-8100 - GetScreenshotCommand now returns screenshot in base64

--- Aeon.Core.Web/src/main/java/aeon/core/command/execution/commands/web/GetScreenshotCommand.java
+++ Aeon.Core.Web/src/main/java/aeon/core/command/execution/commands/web/GetScreenshotCommand.java
@@ -2,7 +2,15 @@ package aeon.core.command.execution.commands.web;
 
 import aeon.core.command.execution.commands.CommandWithReturn;
 import aeon.core.common.Resources;
+import aeon.core.common.exceptions.ScreenshotException;
 import aeon.core.framework.abstraction.drivers.IDriver;
+
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Get screenshot.
@@ -22,6 +30,13 @@ public class GetScreenshotCommand extends CommandWithReturn {
             throw new IllegalArgumentException("driver");
         }
 
-        return driver.getScreenshot();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write((BufferedImage) driver.getScreenshot(), "png", baos);
+        } catch (IOException e) {
+            throw new ScreenshotException("Unable to convert screenshot.");
+        }
+
+        return DatatypeConverter.printBase64Binary(baos.toByteArray());
     }
 }
