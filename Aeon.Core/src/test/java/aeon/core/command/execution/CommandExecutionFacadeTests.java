8fe6663fad76722abfe0627ec3f6b596c3686b9b
Gavril Rodriguez-Thompson <gavrilr@a44436gavrilr.us.corp>
2019-01-14 09:24:21 -0500

    TOOL-8201-Added CommandExecutionFacade unit tests

--- Aeon.Core/src/test/java/aeon/core/command/execution/CommandExecutionFacadeTests.java
+++ Aeon.Core/src/test/java/aeon/core/command/execution/CommandExecutionFacadeTests.java
@@ -1,6 +1,8 @@
 package aeon.core.command.execution;
 
 import aeon.core.command.execution.commands.Command;
+import aeon.core.command.execution.commands.CommandWithReturn;
+import aeon.core.command.execution.consumers.interfaces.IDelegateRunner;
 import aeon.core.command.execution.consumers.interfaces.IDelegateRunnerFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,11 +13,15 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.STRICT_STUBS)
 public class CommandExecutionFacadeTests {
 
+
+    @Mock
+    private IDelegateRunner delegateRunner;
 
     @Mock
     private AutomationInfo automationInfo;
@@ -24,17 +30,20 @@ public class CommandExecutionFacadeTests {
     private Command command;
 
     @Mock
+    private CommandWithReturn returnCommand;
+
+    @Mock
     private IDelegateRunnerFactory delegateRunnerFactory;
 
     private CommandExecutionFacade executionFacade;
 
     @BeforeEach
-    public void setup() {
+    public void setUp() {
         executionFacade = new CommandExecutionFacade(this.delegateRunnerFactory);
     }
 
     @Test
-    public void testExecuteNullCommand() {
+    public void execute_NullCommand_IllegalArgumentException() {
         //arrange
         command = null;
         //act
@@ -43,12 +52,39 @@ public class CommandExecutionFacadeTests {
     }
 
     @Test
-    public void testExecuteCommand() {
+    public void execute_ObjectNullCommand_IllegalArgumentException() {
+        //arrange
+        returnCommand = null;
+        //act
+        assertThrows(IllegalArgumentException.class, () -> executionFacade.execute(automationInfo, returnCommand));
+        //assert
+    }
+
+    @Test
+    public void execute_MockObjects_callsDelegateFactoryCreateInstance() {
+        //arrange
+        when(delegateRunnerFactory.createInstance(automationInfo)).thenReturn(mock(IDelegateRunner.class));
+
+        //act
+        executionFacade.execute(automationInfo, command);
+
+        //assert
+        verify(delegateRunnerFactory.createInstance(automationInfo), times(1)).execute(command.getCommandDelegate());
 
     }
 
     @Test
-    public void testExecuteObject() {
+    public void execute_MockObjects_returnsDelegateFactoryCreateInstance() {
+        //arrange
+        when(delegateRunnerFactory.createInstance(automationInfo)).thenReturn(mock(IDelegateRunner.class));
+
+
+        //act
+        executionFacade.execute(automationInfo, returnCommand);
+
+        //assert
+        //verify(delegateRunner, times(1)).execute(returnCommand.getCommandDelegate());
+        verify(delegateRunnerFactory.createInstance(automationInfo), times(1)).execute(refEq(returnCommand.getCommandDelegate()));
 
     }
 
