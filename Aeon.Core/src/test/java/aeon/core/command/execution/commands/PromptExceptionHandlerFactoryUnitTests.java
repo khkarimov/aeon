75d644947fd90684729640a17ed7e6944481c14b
Ivan Brenes <ivan_brenes@ultimatesoftware.com>
2019-01-16 18:06:20 -0500

    Changed assertions to check if they are instances of certain classes, along with cleaning up the code a bit.

--- Aeon.Core/src/test/java/aeon/core/command/execution/commands/PromptExceptionHandlerFactoryUnitTests.java
+++ Aeon.Core/src/test/java/aeon/core/command/execution/commands/PromptExceptionHandlerFactoryUnitTests.java
@@ -1,58 +1,54 @@
 package aeon.core.command.execution.commands;
 
+import aeon.core.command.execution.consumers.ContinueAtUserExceptionHandler;
 import aeon.core.command.execution.consumers.PromptExceptionHandlerFactory;
+import aeon.core.command.execution.consumers.RethrowExceptionHandler;
 import aeon.core.command.execution.consumers.interfaces.IExceptionHandler;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class PromptExceptionHandlerFactoryUnitTests {
-    public boolean testUserPromptToContinueDecisions;
     public Class testException;
     IExceptionHandler handler;
 
     @Test
     public void getPromptUserForContinueDecision_true_returnTrue() {
         //Arrange
-        testUserPromptToContinueDecisions = true;
-        PromptExceptionHandlerFactory testPromptExceptionHandlerFactory = new PromptExceptionHandlerFactory(testUserPromptToContinueDecisions);
+        PromptExceptionHandlerFactory testPromptExceptionHandlerFactory = new PromptExceptionHandlerFactory(true);
 
         //Act
         boolean prompt = testPromptExceptionHandlerFactory.getPromptUserForContinueDecision();
 
         //Assert
-        assertTrue("The get method seems to work", prompt);
+        assertTrue(prompt);
     }
 
     @Test
     public void createHandlerFor_true_returnContinueAtUserExceptionHandler() {
         //Arrange
-        testUserPromptToContinueDecisions = true;
-        PromptExceptionHandlerFactory testPromptExceptionHandlerFactory = new PromptExceptionHandlerFactory(testUserPromptToContinueDecisions);
+        PromptExceptionHandlerFactory testPromptExceptionHandlerFactory = new PromptExceptionHandlerFactory(true);
 
         //Act
         handler = testPromptExceptionHandlerFactory.createHandlerFor(testException);
 
         //Assert
-        assertTrue("True, returns continue at user exception handler", true);
-        assertNotNull("Handler is not null!", handler);
+        assertThat(handler, instanceOf(ContinueAtUserExceptionHandler.class));
     }
 
     @Test
     public void createHandlerFor_false_returnRethrowExceptionHandler() {
         //Arrange
-        testUserPromptToContinueDecisions = false;
-        PromptExceptionHandlerFactory testPromptExceptionHandlerFactory = new PromptExceptionHandlerFactory(testUserPromptToContinueDecisions);
+        PromptExceptionHandlerFactory testPromptExceptionHandlerFactory = new PromptExceptionHandlerFactory(false);
 
         //Act
         handler = testPromptExceptionHandlerFactory.createHandlerFor(testException);
 
         //Assert
-        assertFalse("False, returns rethrow exception handler", false);
-        assertNotNull("Handler is not null!", handler);
+        assertThat(handler, instanceOf(RethrowExceptionHandler.class));
     }
 
 }
