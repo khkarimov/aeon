8681860ef33551b4eff8f44c4f8affdbbbf39529
Ivan Rojas <ivanro@a38738ivanro.us.corp>
2019-01-25 10:33:46 -0500

    mocked exceptionHandler and verified handle(e) was called on the correct exception using argumentCaptor

--- Aeon.Core/src/test/java/aeon/core/command/execution/consumers/ExceptionHandlingDelegateRunnerTests.java
+++ Aeon.Core/src/test/java/aeon/core/command/execution/consumers/ExceptionHandlingDelegateRunnerTests.java
@@ -1,13 +1,14 @@
 package aeon.core.command.execution.consumers;
 
 import aeon.core.command.execution.consumers.interfaces.IDelegateRunner;
+import aeon.core.command.execution.consumers.interfaces.IExceptionHandler;
 import aeon.core.command.execution.consumers.interfaces.IExceptionHandlerFactory;
 import aeon.core.common.exceptions.TimeoutExpiredException;
 import aeon.core.framework.abstraction.drivers.IDriver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -16,6 +17,7 @@ import org.mockito.quality.Strictness;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,12 +28,15 @@ class ExceptionHandlingDelegateRunnerTests {
 
     @Mock
     private IDelegateRunner successor;
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    @Mock
     private IExceptionHandlerFactory exceptionHandlerFactory;
     @Mock
     private Consumer<IDriver> consumerCommandDelegate;
     @Mock
     private Function<IDriver, Object> functionCommandDelegate;
+    @Mock
+    private IExceptionHandler exceptionHandler;
+
 
     @BeforeEach
     void setup() {
@@ -42,24 +47,38 @@ class ExceptionHandlingDelegateRunnerTests {
     void execute_ConsumerTimeoutExpired_isCaught() {
         //Arrange
         doThrow(TimeoutExpiredException.class).when(successor).execute(consumerCommandDelegate);
+        when(exceptionHandlerFactory.createHandlerFor(TimeoutExpiredException.class)).thenReturn(exceptionHandler);
 
         //Act
         exceptionHandlingDelegateRunner.execute(consumerCommandDelegate);
 
         //Assert
+        ArgumentCaptor<TimeoutExpiredException> ac = ArgumentCaptor.forClass(TimeoutExpiredException.class);
+
         verify(exceptionHandlerFactory).createHandlerFor(TimeoutExpiredException.class);
+        verify(exceptionHandler).handle(ac.capture());
+
+        assertEquals(TimeoutExpiredException.class, ac.getValue().getClass());
+
+
     }
 
     @Test
     void execute_ConsumerRuntimeException_isCaught() {
         //Arrange
         doThrow(RuntimeException.class).when(successor).execute(consumerCommandDelegate);
+        when(exceptionHandlerFactory.createHandlerFor(RuntimeException.class)).thenReturn(exceptionHandler);
 
         //Act
         exceptionHandlingDelegateRunner.execute(consumerCommandDelegate);
 
         //Assert
+        ArgumentCaptor<RuntimeException> ac = ArgumentCaptor.forClass(RuntimeException.class);
+
         verify(exceptionHandlerFactory).createHandlerFor(RuntimeException.class);
+        verify(exceptionHandler).handle(ac.capture());
+
+        assertEquals(RuntimeException.class, ac.getValue().getClass());
     }
 
     @Test
@@ -70,6 +89,7 @@ class ExceptionHandlingDelegateRunnerTests {
         exceptionHandlingDelegateRunner.execute(consumerCommandDelegate);
 
         //Assert
+        verify(successor).execute(consumerCommandDelegate);
         verifyZeroInteractions(exceptionHandlerFactory);
     }
 
@@ -77,24 +97,36 @@ class ExceptionHandlingDelegateRunnerTests {
     void execute_FunctionTimeoutExpired_isCaught() {
         //Arrange
         doThrow(TimeoutExpiredException.class).when(successor).execute(functionCommandDelegate);
+        when(exceptionHandlerFactory.createHandlerFor(TimeoutExpiredException.class)).thenReturn(exceptionHandler);
 
         //Act
         exceptionHandlingDelegateRunner.execute(functionCommandDelegate);
 
         //Assert
+        ArgumentCaptor<TimeoutExpiredException> ac = ArgumentCaptor.forClass(TimeoutExpiredException.class);
+
         verify(exceptionHandlerFactory).createHandlerFor(TimeoutExpiredException.class);
+        verify(exceptionHandler).handle(ac.capture());
+
+        assertEquals(TimeoutExpiredException.class, ac.getValue().getClass());
     }
 
     @Test
     void execute_FunctionRuntimeException_isCaught() {
         //Arrange
         doThrow(RuntimeException.class).when(successor).execute(functionCommandDelegate);
+        when(exceptionHandlerFactory.createHandlerFor(RuntimeException.class)).thenReturn(exceptionHandler);
 
         //Act
         exceptionHandlingDelegateRunner.execute(functionCommandDelegate);
 
         //Assert
+        ArgumentCaptor<RuntimeException> ac = ArgumentCaptor.forClass(RuntimeException.class);
+
         verify(exceptionHandlerFactory).createHandlerFor(RuntimeException.class);
+        verify(exceptionHandler).handle(ac.capture());
+
+        assertEquals(RuntimeException.class, ac.getValue().getClass());
     }
 
     @Test
@@ -105,6 +137,8 @@ class ExceptionHandlingDelegateRunnerTests {
         exceptionHandlingDelegateRunner.execute(functionCommandDelegate);
 
         //Assert
+        verify(successor).execute(functionCommandDelegate);
         verifyZeroInteractions(exceptionHandlerFactory);
+
     }
 }
