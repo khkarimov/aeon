d308e9ef7cd9237ce54fa932ddc3795105c4327b
Gavril Rodriguez-Thompson <gavrilr@a44436gavrilr.us.corp>
2019-01-25 16:36:34 -0500

    TOOL-8249-Modified PCFRunnerService and Docker script

--- Aeon.Platform.Http/docker/create-docker-image.sh
+++ Aeon.Platform.Http/docker/create-docker-image.sh
@@ -1,8 +1,10 @@
 cd ../../
+./gradlew assemble
 ./gradlew Aeon.Platform.Http:shadowJar
 cd Aeon.Platform.Http/docker
 
-cp ../build/libs/*.jar .
+cp ../build/libs/Aeon.Platform.Http-*-all.jar aeon.jar
+.jar aeon.jar
 cp -r ../lib .
 cp -r ../plugins .
 docker build -t aeon-runner .
