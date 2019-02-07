7b03bb4269575342be2ce872f58bcdf45089714a
Gavril Rodriguez-Thompson <gavrilr@a44436gavrilr.us.corp>
2019-02-07 16:28:01 -0500

    TOOL-8305-AddedTabWebElement

---
+++ Aeon.Core.Web/src/main/java/aeon/core/testabstraction/elements/web/Tab.java
@@ -0,0 +1,38 @@
+package aeon.core.testabstraction.elements.web;
+
+import aeon.core.command.execution.AutomationInfo;
+import aeon.core.common.web.interfaces.IByWeb;
+
+/**
+ * The class to model Tab elements.
+ */
+public class Tab extends WebElement {
+
+    private AutomationInfo info;
+    private IByWeb selector;
+
+    /**
+     * Creates a new instance of {@link Tab}.
+     *
+     * @param info     The automation info.
+     * @param selector IBy selector that will identify the element.
+     */
+    public Tab(AutomationInfo info, IByWeb selector) {
+        super(info, selector);
+        this.info = info;
+        this.selector = selector;
+    }
+
+    /**
+     * Creates a new instance of {@link Tab}.
+     *
+     * @param info            The automation info.
+     * @param selector        IBy selector that will identify the element.
+     * @param switchMechanism The switch mechanism.
+     */
+    public Tab(AutomationInfo info, IByWeb selector, Iterable<IByWeb> switchMechanism) {
+        super(info, selector, switchMechanism);
+        this.info = info;
+        this.selector = selector;
+    }
+}
