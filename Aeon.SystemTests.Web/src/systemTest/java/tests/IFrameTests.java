8f0a198ec2454710c00dcaa8714c9f2cfb49e817
Ivan Brenes <ivan_brenes@ultimatesoftware.com>
2019-03-08 14:11:57 -0500

    Adding in tests and making the html box bigger so that it passes when aeon tries to click on the searach icon

--- Aeon.SystemTests.Web/src/systemTest/java/tests/IFrameTests.java
+++ Aeon.SystemTests.Web/src/systemTest/java/tests/IFrameTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import aeon.core.testabstraction.product.WebConfiguration;
 import main.iframe.IFrameWikiSample;
+import main.iframe.NestedIFrameWikiSample;
+import main.iframe.pages.NestedIFramePage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,35 +14,36 @@ import static aeon.core.testabstraction.product.Aeon.launch;
  * This test file was created to show Aeon working on a UltiPro environment and to test the switch mechanism.
  */
 public class IFrameTests {
-    private static IFrameWikiSample product;
+    private static IFrameWikiSample wikiPage;
+    private static NestedIFrameWikiSample nestedWikiPage;
 
     @Before
-    public void beforeTests() {
-        product = launch(IFrameWikiSample.class);
-        String environment = product.getConfig(WebConfiguration.Keys.ENVIRONMENT,
-                "/" + System.getProperty("user.dir").replace('\\', '/') + "/Test-Sample-Context/iFrame_clean.html");
-        String protocol = product.getConfig(WebConfiguration.Keys.PROTOCOL, "file");
-        product.browser.goToUrl(protocol + "://" + environment);
+    public void beforeIFrameTests() {
+        wikiPage = launch(IFrameWikiSample.class);
+        String environment = wikiPage.getConfig(WebConfiguration.Keys.ENVIRONMENT,
+                "/" + System.getProperty("user.dir").replace('\\', '/') + "/Test-Sample-Context/iFrame.html");
+        String protocol = wikiPage.getConfig(WebConfiguration.Keys.PROTOCOL, "file");
+        wikiPage.browser.goToUrl(protocol + "://" + environment);
     }
 
     @After
-    public void afterTests() {
-        product.browser.quit();
+    public void afterIFrameTests() {
+        wikiPage.browser.quit();
     }
 
     @Test
     public void iFrameWithSwitchTest(){
-        product.iFramePage.wikiSearchTextBox.click();
-        product.iFramePage.searchButton.click();
-        product.iFramePage.wikiSearchTextBox.click();
-        product.iFramePage.wikiSearchTextBox.set("Ultimate Software");
-        product.iFramePage.searchButton.click();
-        product.iFramePage.wikiLogo.click();
-        product.iFramePage.popupButton.click();
-        product.browser.switchToWindowByTitle("Bing");
-        product.browser.verifyTitle("Bing");
-        product.browser.switchToWindowByTitle("Title");
-        product.browser.verifyTitle("Title");
+        wikiPage.iFramePage.wikiSearchTextBox.click();
+        wikiPage.iFramePage.searchButton.click();
+        wikiPage.iFramePage.wikiSearchTextBox.click();
+        wikiPage.iFramePage.wikiSearchTextBox.set("Ultimate Software");
+        wikiPage.iFramePage.searchButton.click();
+        wikiPage.iFramePage.wikiLogo.click();
+        wikiPage.iFramePage.popupButton.click();
+        wikiPage.browser.switchToWindowByTitle("Bing");
+        wikiPage.browser.verifyTitle("Bing");
+        wikiPage.browser.switchToWindowByTitle("Title");
+        wikiPage.browser.verifyTitle("Title");
     }
 }
 
