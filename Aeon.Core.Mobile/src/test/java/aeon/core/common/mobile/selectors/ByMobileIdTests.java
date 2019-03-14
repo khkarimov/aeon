1ff1f145f68ca3f66ec53ce79b8ed2368de29670
arcticfaded <jbelt021@fiu.edu>
2019-03-14 19:01:18 -0400

    ByMobileId: Added test coverage for mobile selectors

---
+++ Aeon.Core.Mobile/src/test/java/aeon/core/common/mobile/selectors/ByMobileIdTests.java
@@ -0,0 +1,83 @@
+package aeon.core.common.mobile.selectors;
+
+import aeon.core.common.mobile.interfaces.IByMobile;
+import aeon.core.common.web.interfaces.IByWeb;
+import aeon.core.common.web.selectors.ByJQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+public class ByMobileIdTests {
+    private ByMobileId command;
+
+    private String mySelector = "My Selector";
+
+    @BeforeEach
+    public void setup() {
+        command = new ByMobileId(mySelector);
+    }
+
+
+    @Test
+    public void createMobileSelector_whenSelectorIsCreatedUsingID_returnsCorrectId() {
+        //Arrange
+        String myId = "new ID";
+
+        //Act
+        IByMobile myNewSelector = ByMobileId.id(myId);
+
+        //Assert
+        assertEquals("When instantiating using id, returns correct id", ((ByMobileId) myNewSelector).getSelector(), myId);
+    }
+
+    @Test
+    public void useMobileSelector_whenGettingSelector_returnsCorrectId(){
+        //Arrange
+
+        //Act
+        String selector = command.getSelector();
+
+        //Assert
+        assertEquals("When instantiating using constructor, returns correct id", selector, mySelector);
+    }
+
+    @Test
+    public void useMobileSelector_whenGettingToString_returnsCorrectId(){
+        //Arrange
+
+        //Act
+        String selector = command.toString();
+
+        //Assert
+        assertEquals("When instantiating using constructor, returns correct id", selector, mySelector);
+    }
+
+
+    @Test
+    public void useMobileSelector_whenUsingAnIByWebSelector_throwsException(){
+        //Arrange
+        IByWeb myFindSelector = new ByJQuery("#my-jquery-selector");
+
+        //Act and Assert
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            command.find(myFindSelector);
+        });
+    }
+
+    @Test
+    public void useMobileSelector_whenRetrievingJquerySelector_throwsException(){
+
+        //Act and Assert
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            command.toJQuery();
+        });
+    }
+}
