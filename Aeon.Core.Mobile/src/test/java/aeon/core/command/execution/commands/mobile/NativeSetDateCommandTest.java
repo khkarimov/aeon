d5de4e4e3e34ad1178ea73a7da4b478fe92cf6a6
Kaden King <kaden_king@ultimatesoftware.com>
2019-03-14 18:38:33 -0400

    added bad date test

--- Aeon.Core.Mobile/src/test/java/aeon/core/command/execution/commands/mobile/NativeSetDateCommandTest.java
+++ Aeon.Core.Mobile/src/test/java/aeon/core/command/execution/commands/mobile/NativeSetDateCommandTest.java
@@ -1,15 +1,17 @@
 package aeon.core.command.execution.commands.mobile;
 
 import aeon.core.framework.abstraction.drivers.IMobileDriver;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -22,20 +24,27 @@ class NativeSetDateCommandTest {
 
     private NativeSetDateCommand nativeSetDateCommand;
 
-    @BeforeEach
-    public void setUp() {
-        this.nativeSetDateCommand = new NativeSetDateCommand("1972-11-11");
-    }
-
     @Test
     public void driverDelegate_happyPath_setsDate() {
         // Arrange
-        LocalDate expecedDate = LocalDate.parse("1972-11-11");
+        LocalDate expectedDate = LocalDate.parse("1972-11-11");
 
         // Act
+        this.nativeSetDateCommand = new NativeSetDateCommand("1972-11-11");
         nativeSetDateCommand.driverDelegate(iMobileDriverMock);
 
         // Assert
-        verify(iMobileDriverMock, times(1)).setDate(expecedDate);
+        verify(iMobileDriverMock, times(1)).setDate(expectedDate);
+    }
+
+    @Test
+    public void driverDelegate_notADate_throwsDateTimeParseException() {
+        // Arrange
+
+        // Act
+        Executable action = () -> this.nativeSetDateCommand = new NativeSetDateCommand("bad date");
+
+        // Assert
+        Assertions.assertThrows(DateTimeParseException.class, action);
     }
 }
