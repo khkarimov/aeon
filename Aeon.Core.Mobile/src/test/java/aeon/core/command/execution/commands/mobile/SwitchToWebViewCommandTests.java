b91f0e8d2e710f843985930d31c9bf08378a9f35
Daniel Gonzalez <danny_gonzalez@ultimatesoftware.com>
2019-03-14 19:10:38 -0400

    added switch to web view command unit tests

---
+++ Aeon.Core.Mobile/src/test/java/aeon/core/command/execution/commands/mobile/SwitchToWebViewCommandTests.java
@@ -0,0 +1,40 @@
+package aeon.core.command.execution.commands.mobile;
+
+
+import aeon.core.common.web.interfaces.IByWeb;
+import aeon.core.framework.abstraction.drivers.IMobileDriver;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+public class SwitchToWebViewCommandTests {
+
+    @Mock
+    IByWeb mockSelector;
+
+    @Mock
+    IMobileDriver mockDriver;
+
+    private SwitchToWebViewCommand command;
+
+    @Test
+    void driverDelegate_HappyPath() {
+
+        //Arrange
+        command = new SwitchToWebViewCommand(mockSelector);
+
+        //Act
+        command.driverDelegate(mockDriver);
+
+        //Assert
+        verify(mockDriver, times(1)).switchToWebView(mockSelector);
+    }
+}
